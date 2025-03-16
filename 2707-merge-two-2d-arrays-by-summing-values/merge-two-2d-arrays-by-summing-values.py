cf58dce0f91b408a0257999dc87c83520ea811ea
Suthichai <sutichai2542@gmail.com>
2025-03-17 03:40:46 +0700

    Time: 0 ms (100.00%) | Memory: 17.9 MB (62.62%) - LeetSync

--- 2707-merge-two-2d-arrays-by-summing-values/merge-two-2d-arrays-by-summing-values.py
+++ 2707-merge-two-2d-arrays-by-summing-values/merge-two-2d-arrays-by-summing-values.py
@@ -1,17 +1,7 @@
 class Solution:
     def mergeArrays(self, nums1: List[List[int]], nums2: List[List[int]]) -> List[List[int]]:
-        if len(nums1) < len(nums2):
-            minArrays = nums1
-            maxArrays = nums2
-        else:
-            minArrays = nums2
-            maxArrays = nums1
-
         values = {}
-        for id, value in minArrays:
-            values[id] = value
-
-        for id, value in maxArrays:
+        for id, value in (nums1 + nums2):
             currentValue = values.get(id, 0)
             values[id] = currentValue + value
 
